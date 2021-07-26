15a2fb79745cae24c6ac5353c8e13ddd7fb6934d
Hellshi <hcslimaa@gmail.com>
2021-07-26 11:41:20 -0300

    Fetching data from the OpenWeather.com

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location'
 
+const API_KEY = '7290651dc0b266597e031812f9623844'
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?'
+
 export default function App() {
   const [error, setError] = useState(null)
+  const [wheater, setWheater] = useState([])
   useEffect(() => {
     Load()
   }, [])
@@ -19,21 +23,32 @@ export default function App() {
       }
       const location = await Location.getCurrentPositionAsync()
       const { latitude, longitude } = location.coords
-      alert(`${latitude} ${longitude}`)
+      const wheatherURL = `${baseURL}lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      const response = await fetch(wheatherURL)
+      const result = await response.json()
+      if(response.ok) {
+        setWheater(result)
+      } else {
+        setError(result.message)
+      }
     }catch(error){}
   }
-  return (
+  if(wheater) {
+    const { main } = wheater
+      return (
     <View style={styles.container}>
-      <Text>Hello from the deepts of hell</Text>
+      <Text>{main.temp}</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
+  }
+;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'pink',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
